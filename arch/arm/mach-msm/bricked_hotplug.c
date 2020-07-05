3d26cc2cb73ff8c605bfcc7a54112f2c5c817f31
wlkmanist <wlkmanist@gmail.com>
2020-07-06 02:59:31 +0300

    bricked hotplug tweaks
    Signed-off-by: wlkmanist <wlkmanist@gmail.com>

--- arch/arm/mach-msm/bricked_hotplug.c
+++ arch/arm/mach-msm/bricked_hotplug.c
@@ -83,7 +83,7 @@ static struct cpu_hotplug {
 	.hotplug_suspend = 0,
 };
 
-static unsigned int NwNs_Threshold[8] = {12, 0, 25, 7, 30, 10, 0, 18};
+static unsigned int NwNs_Threshold[8] = {12, 0, 20, 7, 25, 10, 0, 18};
 static unsigned int TwTs_Threshold[8] = {140, 0, 140, 190, 140, 190, 0, 190};
 
 struct down_lock {
