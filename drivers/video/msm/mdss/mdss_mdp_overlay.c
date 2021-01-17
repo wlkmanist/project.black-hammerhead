36264fb55a84d75c226f10d95b3475e1974b813e
wlkmanist <t.me/wlkmanist>
2021-01-18 00:06:02 +0300

    mdss: previous cherry-pick fix
    Signed-off-by: wlkmanist <t.me/wlkmanist>

--- drivers/video/msm/mdss/mdss_mdp_overlay.c
+++ drivers/video/msm/mdss/mdss_mdp_overlay.c
@@ -2228,7 +2228,6 @@ __vsync_retire_get_fence(struct msm_sync_pt_data *sync_pt_data)
 static int __vsync_retire_setup(struct msm_fb_data_type *mfd)
 {
 	struct mdss_overlay_private *mdp5_data = mfd_to_mdp5_data(mfd);
-	struct sched_param param = { .sched_priority = 5 };
 	char name[24];
 	struct sched_param param = { .sched_priority = 5 };
 
@@ -2254,18 +2253,6 @@ static int __vsync_retire_setup(struct msm_fb_data_type *mfd)
  	sched_setscheduler(mdp5_data->thread, SCHED_FIFO, &param);
 
 	mfd->mdp_sync_pt_data.get_retire_fence = __vsync_retire_get_fence;
-
-	init_kthread_worker(&mdp5_data->worker);
-	init_kthread_work(&mdp5_data->vsync_work, __vsync_retire_work_handler);
-
-	mdp5_data->thread = kthread_run(kthread_worker_fn,
-				&mdp5_data->worker, "vsync_retire_work");
-	if (IS_ERR_OR_NULL(mdp5_data->thread)) {
-		pr_err("Unable to start vsync thread\n");
-		return -ENOMEM;
-	}
-
-	sched_setscheduler(mdp5_data->thread, SCHED_FIFO, &param);
 
 	mdp5_data->vsync_retire_handler.vsync_handler =
 		__vsync_retire_handle_vsync;
