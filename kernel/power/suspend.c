864922103e4b8c1099cf9df17867731202961db2
wlkmanist <t.me/wlkmanist>
2021-01-24 23:57:26 +0300

    Revert "ftrace: Disable function tracing during suspend/resume and hibernation, again"
    This reverts commit ae1436a87d86362025d31af6346c6e9f7fbbd9ba.

--- kernel/power/suspend.c
+++ kernel/power/suspend.c
@@ -24,7 +24,6 @@
 #include <linux/export.h>
 #include <linux/suspend.h>
 #include <linux/syscore_ops.h>
-#include <linux/ftrace.h>
 #include <linux/rtc.h>
 #include <linux/wakeup_reason.h>
 #include <linux/partialresume.h>
@@ -294,7 +293,6 @@ int suspend_devices_and_enter(suspend_state_t state)
 			goto Close;
 	}
 	suspend_console();
-	ftrace_stop();
 	suspend_test_start();
 	error = dpm_suspend_start(PMSG_SUSPEND);
 	if (error) {
@@ -315,7 +313,6 @@ int suspend_devices_and_enter(suspend_state_t state)
 	if (!resumed)
 		dpm_resume_end(PMSG_RESUME);
 	suspend_test_finish("resume devices");
-	ftrace_start();
 	resume_console();
  Close:
 	if (suspend_ops->end)
